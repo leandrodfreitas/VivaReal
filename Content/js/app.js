8e1fd737ad57b6b8440bda448bebdc4e79465661
leandrodfreitas@gmail.com <leandrodfreitas@gmail.com>
2016-09-21 20:18:45 -0300

    Inclusao de loading.gif

--- Content/js/app.js
+++ Content/js/app.js
@@ -2,9 +2,14 @@ var app = angular.module("app",[]);
 
 app.controller('imoveisController', function($scope, $http, $filter) {
 	
+	$scope.loading = true;
+	
    $http.get('http://spotippos.vivareal.com/properties?ax=1&ay=1&bx=1400&by=1000')
 	.success(function(data){		
 		$scope.lista = data.properties;
-	});
+	})
+	.finally(function () {
+      $scope.loading = false;
+   });
 	
 });
